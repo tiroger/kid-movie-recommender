79d5ff742914e2e18f4c7b9b192f7f43253aa3cf
Roger Lefort <tirogerl@gmail.com>
2023-03-28 17:25:35 -0700

    scraping script

---
+++ scrape_database.py
@@ -0,0 +1,20 @@
+#############
+# LIBRARIES #
+#############
+
+import os
+
+import requests
+from bs4 import BeautifulSoup as bs
+imoprt bs4.element
+
+
+import pandas as pd
+
+
+
+###################
+# SCAPING SCRIPTS #
+###################
+
+URI = "https://www.football-data.co.uk/mmz4281/1920/E0.csv"
